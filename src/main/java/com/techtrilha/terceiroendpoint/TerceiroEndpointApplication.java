91a2637be95ca814595b684b0b4a45543e2c7476
Felipe Miranda <felip.simi@gmail.com>
2020-06-17 20:28:09 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/main/java/com/techtrilha/terceiroendpoint/TerceiroEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.techtrilha.terceiroendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerceiroEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerceiroEndpointApplication.class, args);
+	}
+
+}
